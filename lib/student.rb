fc7ed633af97755c187338e6bc81d99c62175db8
Cailie Skelton <cailie.skelton86@gmail.com>
2020-09-27 19:39:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
      sql = <<-SQL
       SELECT * 
       FROM students
-      WHERE grade = 9
+      WHERE students.grade = 9
     SQL
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
