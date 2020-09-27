ccd4532a4712c614351f09da9144d6a7c1f9c56c
Cailie Skelton <cailie.skelton86@gmail.com>
2020-09-27 19:51:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,17 +53,17 @@ class Student
     end
    end
    
-  # def self.first_X_students_in_grade_10
-  #       sql = <<-SQL
-  #     SELECT * 
-  #     FROM students
-  #     WHERE students.grade = 10 
-  #     LIMIT 1
-  #   SQL
-  #   DB[:conn].execute(sql).map do |row|
-  #     self.new_from_db(row)
-  #   end
-  # end
+  def self.first_X_students_in_grade_10
+        sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE students.grade = 10 
+      LIMIT X
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
   
   def save
     sql = <<-SQL
